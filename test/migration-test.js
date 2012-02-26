64c7115f89039af275685481ab8205de953f1f9b
Brian J Brennan <brianloveswords@gmail.com>
2012-02-25 23:49:01 -0500

    Working on migrations: dropping columns

--- test/migration-test.js
+++ test/migration-test.js
@@ -2,28 +2,55 @@ var _ = require('underscore')
   , vows = require('vows')
   , assert = require('assert')
   , should = require('should')
+  , fmap = require('functools').map
   
   , common = require('./common.js')
   , Hyde = require('..')(common.conf)
   , Base = Hyde.Base
   , client = Hyde.client
 
+
+
 common.prepareTesting(client);
+
+var Models = {
+  Volatile: Base.extend({
+    table: 'volatile',
+    schema: { id: Base.Schema.Id, drop: Base.Schema.Number, rename: Base.Schema.Number }
+  }),
+  
+  User: Base.extend({
+    table: 'user',
+    schema: { id: Base.Schema.Id }
+  }),
+  
+  Stuff: Base.extend({
+    table: 'stuff',
+    schema: { id: Base.Schema.Id }
+  }),
+};
+
+function binder(o) { _.bindAll(o); }
+_.map(_.values(Models), binder);
+      
+
 vows.describe('testing migrations').addBatch({
   'Test some migrations, yo' : {
     topic: function () {
-      var User = Base.extend({
-        table: 'user',
-        schema: { id: Base.Schema.Id }
-      });
+      var maker = function (M, callback) { M.makeTable(callback) }
       
-      return User;
+      fmap.async(maker, _.values(Models), function (err, res) {
+        if (err) throw err;
+        this.callback(null, Models);
+      }.bind(this));
+    
+    
     },
     // 'getCreateTable': function () {
     //   var t = Base.Migration(User);
     // },
     'getAlterSql': {
-      topic: function (User) { return User.Migration(User) },
+      topic: function (M) { return M.User.Migration() },
       'add' : function (t) {
         var sql = t.getAlterSql({ yam: Base.Schema.Text }, 'add');
         sql[0].should.equal('ALTER TABLE `user` ADD `yam` TEXT');
@@ -45,7 +72,7 @@ vows.describe('testing migrations').addBatch({
       },
       'drop': function (t) {
         var sql = t.getAlterSql('words', 'drop');
-        sql[0].should.equal('ALTER TABLE `user` DROP `words`');
+        sql[0].should.equal('ALTER TABLE `user` DROP COLUMN `words`');
       },
       'engine': function (t) {
         var sql = t.getAlterSql('MyISAM', 'engine');
@@ -63,14 +90,113 @@ vows.describe('testing migrations').addBatch({
       }
     },
     'addColumn' : {
-      topic: function (User) {
-        var t = Base.Migration(User);
-        t.addColumn({emperor: Base.Schema.String({default: 'x'})}, this.callback);
+      'simple like' : {
+        topic: function (M) {
+          var t = M.User.Migration();
+          
+          var cb_addCol = function (err, result) {
+            if (err) return this.callback(err);
+            Base.client.query('show create table user', this.callback)
+          }.bind(this);
+          
+          var spec = { emperor: Base.Schema.String({
+            type: 'varchar',length: 255,unique: 128,default: 'x'
+          })}
+          
+          try {
+            t.addColumn(spec, cb_addCol);
+          } catch (err) {
+            this.callback(err);
+          }
+        },
+        'does what it says on the tin' : function (err, result) {
+          assert.ifError(err);
+          var sql = result.pop()['Create Table'];
+          sql.should.match(/unique key `emperor` \(`emperor`\(128\)\)/i);
+          sql.should.match(/`emperor` varchar\(255\) default 'x'/i);
+        }
       },
-      'does what it says on the tin' : function (err, result) {
+      'more complex addColumn' : {
+        topic: function (M) {
+          var t = Base.Migration(M.User);
+          _.bindAll(t);
+          
+          
+          var adder = function (spec, callback) {
+            t.addColumn(spec, callback);
+          }
+          
+          var cb_addCol = function (err, result) {
+            if (err) return this.callback(err);
+            Base.client.query('show create table user', this.callback)
+          }.bind(this);
+          
+          var specs =[
+            { sss: Base.Schema.String({type: 'char', length: 128, unique: true, default: 'y'})},
+            { nnn: Base.Schema.Number() },
+            { eee: Base.Schema.Enum(['one', 'two']) },
+            { stuff_id: Base.Schema.Foreign({model: M.Stuff, field: 'id'})}
+          ]
+          try {
+            fmap.async(adder, specs, cb_addCol);
+          } catch (err) {
+            this.callback(err);
+          }
+        },
+        
+        'adds all the fields fine' : function (err, result) {
+          var sql = result[0]['Create Table'];
+          sql.should.match(/`sss` char\(128\) default 'y'/i);
+          sql.should.match(/`nnn` int\(\d+\) default null/i);
+          sql.should.match(/`eee` enum\('one','two'\) default null/i);
+          sql.should.match(/`stuff_id` (big)?int\(\d+\) default null/i);
+        },
+        
+        'adds all the keys fine' : function (err, result) {
+          var sql = result[0]['Create Table'];
+          sql.should.match(/unique key `sss` \(`sss`\)/i);
+          sql.should.match(/key `stuff_fkey` \(`stuff_id`\)/i);
+          sql.should.match(/constraint `user_ibfk_1` foreign key \(`stuff_id`\) references `stuff` \(`id`\)/i);
+        }
+      }
+    },
+    'dropColumn' : {
+      topic: function (M) {
+        var t = M.Volatile.Migration();
+        var cb = function (err, result) {
+          if (err) return this.callback(err);
+          Hyde.client.query('show create table user', this.callback)
+        }.bind(this);
+        
+        try {
+          t.dropColumn('drop', cb);
+        } catch (error) {
+          this.callback(error);
+        }
+      },
+      'should drop the damn column': function (err, result) {
         assert.ifError(err);
-        result.affectedRows.should.equal(1);
+        result.should.not.match(/drop/);
+      }
+    },
+    'renameColumn' : {
+      topic: function (M) {
+        var t = M.Volatile.Migration();
+        var cb = function (err, result) {
+          if (err) return this.callback(err);
+          Hyde.client.query('show create table user', this.callback)
+        }.bind(this);
+        
+        try {
+          t.dropColumn('drop', cb);
+        } catch (error) {
+          this.callback(error);
+        }
       },
+      'should drop the damn column': function (err, result) {
+        assert.ifError(err);
+        result.should.not.match(/one/);
+      }
     }
   }
 }).export(module);
